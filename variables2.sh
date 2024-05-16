bff8347f3c6818c90d7082769148382106260002
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-05-16 19:35:40 +0200

    third

---
+++ variables2.sh
@@ -0,0 +1,4 @@
+#!/bin/Bash
+read -p " Escribe tu actividad favorita " actividad
+read -p " Escribe tu comida favorita " comida
+echo "Mi actividad favorita es" $actividad "Mi comida favorita es" $comida
