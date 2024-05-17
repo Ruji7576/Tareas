f8eeaea3efd195f3b9a7146dd43b7969160b2050
Ruji7576 <ukrainemaksymhryshchuk@gmail.com>
2024-05-17 12:30:51 +0200

    fifth

--- parametros2.sh
+++ parametros2.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-# Задаємо змінні з назвою файлу і кількістю параметрів
-file_name="data.txt"
-num_parameters=3
-
-# Створюємо речення за допомогою команди echo з використанням змінних
-echo "У файлі $file_name існує $num_parameters параметри."
+echo "En el fichero" $0 "existen" $# 
